b5632d0271ededbcfbb5eaed2c50ce033fdc0520
GarethBergin <garethbergin2003@gmail.com>
2025-11-26 14:31:26 +0000

    Graphs

--- src/datavis.py
+++ src/datavis.py
@@ -1,6 +1,5 @@
 #%% 
 import altair as alt
-#%%
 import pandas as pd
 
 #%% Import data from tables
@@ -15,15 +14,66 @@ df2.head(10)
 
 # %%
 alt.data_transformers.disable_max_rows()
-# load a sample dataset as a pandas DataFrame
-#from altair.datasets import data
-#cars = data.cars()
 
-# make the chart
-alt.Chart(df).mark_point().encode(
+# make the chart for PovertyData
+chart1 = alt.Chart(df).mark_point().encode(
     x='geo',
     y='OBS_VALUE',
     color='sex',
+    tooltip=['TIME_PERIOD', 'sex', 'OBS_VALUE', 'geo']
 ).interactive()
 
+# make the chart for PurchasingPowerParities
+chart2 = alt.Chart(df2).mark_point().encode(
+    x='geo',
+    y='OBS_VALUE',
+    color='ppp_cat',
+    tooltip=['TIME_PERIOD', 'ppp_cat', 'OBS_VALUE', 'geo']
+).interactive()
+
+# %%
+#Scatter Plot graph for PovertyData and PurchasingPowerParities
+chart1 + chart2
+# %%
+#Scatter Plot graph for PovertyData
+chart1
+#%%
+#Scatter Plot graph for PurchasingPowerParities
+chart2
+# %%
+source = df
+source2 = df2
+brush = alt.selection_interval(resolve='global')
+
+base = alt.Chart(source).mark_bar().encode(
+    y='OBS_VALUE',
+    color=alt.Color("sex", legend=alt.Legend(title='Gender')), 
+    tooltip=['TIME_PERIOD', 'sex', 'OBS_VALUE', 'geo']
+).add_params(
+    brush
+).properties(
+    width=250,
+    height=250
+)
+
+base2 = alt.Chart(source2).mark_bar().encode(
+    y='OBS_VALUE',
+    color=alt.Color("ppp_cat", legend=alt.Legend(title='Food Category')), 
+    tooltip=['TIME_PERIOD', 'ppp_cat', 'OBS_VALUE', 'geo']
+).add_params(
+    brush
+).properties(
+    width=250,
+    height=250
+)
+
+#base.encode(x='sex') | base2.encode(x='ppp_cat')
+
+combined_chart = alt.hconcat(
+    base,
+    base2
+).resolve_scale(
+    color='independent'  # Ensure that the color scales are independent
+)
+combined_chart
 # %%
